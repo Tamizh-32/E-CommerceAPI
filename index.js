b9d770083ee3559017010b744256e13bcd485000
Tamizharasan K <tamizharasan2000k@gmail.com>
2025-05-14 14:44:42 +0530

    Update the Product Routes View Products Method

--- index.js
+++ index.js
@@ -43,7 +43,9 @@ app.use(errorHandler);
 chatSocket(io);
 
 // Start server
+
+
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
+server.listen(PORT,'0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -2,7 +2,7 @@ const express=require('express');
 const router=express.Router();
 const productController = require('../controllers/productController');
 
-router.get('/products',productController.updateProduct);
+router.get('/products',productController.viewProduct);
 router.post('/product/add',productController.addProduct);
 router.put('/product/update/:id',productController.updateProduct);
 router.delete('/product/:id',productController.deleteProduct)
